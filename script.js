c8feefc8f2a3f7135ece28eb220e7aa9c71ae42c
Nacko577 <f.alexandru577@gmail.com>
2025-12-02 02:48:29 +0200

    Play Round
    Created playRound function that handles logic in each case and determines a winner

--- script.js
+++ script.js
@@ -13,3 +13,41 @@ function getHumanChoice() {
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if(humanChoice == "rock" && computerChoice == "rock") {
+        console.log("It's a tie!");
+    }
+    if(humanChoice == "rock" && computerChoice == "paper") {
+        console.log("You lose! Paper beats Rock.");
+        computerScore++;
+    }
+    if(humanChoice == "rock" && computerChoice == "scissors") {
+        console.log("You win! Rock beats Scissors.");
+        humanScore++;
+    }
+    if(humanChoice == "paper" && computerChoice == "rock") {
+        console.log("You win! Paper beats Rock.");
+        humanScore++;
+    }
+    if(humanChoice == "paper" && computerChoice == "paper") {
+        console.log("It's a tie!");
+    }
+    if(humanChoice == "paper" && computerChoice == "scissors") {
+        console.log("You lose! Scissors beats Paper.");
+        computerScore++;
+    }
+    if(humanChoice == "scissors" && computerChoice == "rock") {
+        console.log("You lose! Rock beats Scissors.");
+        computerScore++;
+    }
+    if(humanChoice == "scissors" && computerChoice == "paper") {
+        console.log("You win! Scissors beats Paper.");
+        humanScore++;
+    }
+    if(humanChoice == "scissors" && computerChoice == "scissors") {
+        console.log("It's a tie!");
+    }
+}
+
+playRound(getHumanChoice(), getComputerChoice());
