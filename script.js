8fc7c8809bc4912ce60eb5ac312c1fc111758064
Nacko577 <f.alexandru577@gmail.com>
2025-12-02 02:35:36 +0200

    Computer decision
    Updated js with a function that randomly chooses rock, paper, or scissors

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Hello world!");
+function getComputerChoice() {
+    if (Math.random() < 0.33) 
+        return "rock";
+    else if (Math.random() < 0.66 && Math.random() >= 0.33)
+        return "paper";
+    else return "scissors"; 
+}
